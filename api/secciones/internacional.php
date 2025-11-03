f248e38dfe13827a4209c8269a5b2df4f9ae9992
Ilan Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-11-03 22:14:06 +0000

    Actualizar secciones de internacional y nacional con nuevas noticias relevantes

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,20 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  [
+    "titulo" => "Cumbre climática global acuerda metas más ambiciosas",
+    "autor"  => "María López",
+    "resumen"=> "Los representantes de 120 países firmaron un pacto para acelerar la reducción de emisiones y financiar la adaptación en países vulnerables.",
+  ],
+  [
+    "titulo" => "Crisis diplomática: conversaciones entre países vecinos se reanudan",
+    "autor"  => "Andrés Gómez",
+    "resumen"=> "Tras semanas de tensión, los ministros exteriores se reúnen para restablecer canales de diálogo y cooperación comercial.",
+  ],
+  [
+    "titulo" => "Crisis dddd: conversaciones entre países vecinos se reanudan",
+    "autor"  => "Andrés Gómez",
+    "resumen"=> "Tras semanas de tensión, los ministros exteriores se reúnen para restablecer canales de diálogo y cooperación comercial.",
+  ],
 ];
 ?>
--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -2,10 +2,21 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  [
+    "titulo" => "Gobierno anuncia plan de inversión en infraestructura",
+    "autor"  => "Juan Pérez",
+    "resumen"=> "El plan prevé la construcción de carreteras y hospitales en zonas rurales durante los próximos 4 años con financiamiento público-privado.",
+  ],
+  [
+    "titulo" => "Nueva ley educativa avanza en el congreso",
+    "autor"  => "Lucía Fernández",
+    "resumen"=> "La reforma contempla actualización curricular y mayor apoyo a la formación docente; ahora pasa a comisión para evaluación.",
+  ],
+  [
+    "titulo" => "Nueva ley educativa avanza en el congreso",
+    "autor"  => "Lucía Fernández",
+    "resumen"=> "La reforma contempla actualización curricular y mayor apoyo a la formación docente; ahora pasa a comisión para evaluación.",
+  ],
 ];
 ?>
